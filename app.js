34732133d6b18ac30d861b26aaaa59c538cb3298
Wilson Page <wilsonpage@me.com>
2015-05-12 15:14:46 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var init = performance.now();
+var channel;
+var start;
+
+addEventListener('message', onMessage);
+
+function onMessage(e) {
+  switch (e.data) {
+    case 'ready':
+      console.log('ready', performance.now() - init);
+      channel = e.source;
+      connect();
+    break;
+      
+    case 'connected': 
+      channel.postMessage('request', '*');
+    break;
+      
+    case 'response':
+      var now = performance.now();
+      console.log('since init', now - init)
+      console.log('since connect', now - start);
+    break;
+  }
+}
+
+function connect() {
+  start = performance.now();
+  channel.postMessage('connect', '*');
+}
---
+++ service.js
@@ -0,0 +1,16 @@
+addEventListener('message', onMessage);
+
+function onMessage(e) {
+  console.log('child', e.data);
+  
+  switch (e.data) {
+    case 'connect':
+      window.parent.postMessage('connected', '*');
+    break;
+    case 'request': 
+      window.parent.postMessage('response', '*');
+    break;
+  }
+}
+
+window.parent.postMessage('ready', '*');
